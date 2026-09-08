72a3908b238ab0ea7e80c75cc03b195a0cc0e797
Chaosflofan <grossermaximilian001@gmail.com>
2026-09-08 23:04:32 +0200

    Add root build.gradle with all dependencies for JARVIS

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build.gradle.kts for JARVIS-Android
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
